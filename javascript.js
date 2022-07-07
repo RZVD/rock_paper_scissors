b668f5ff73c8a1329cd4f807f3c9ea106088551f
RZVD <razvanfuca@gmail.com>
2022-07-07 17:50:17 +0300

    Scoretracking bugfixes

--- javascript.js
+++ javascript.js
@@ -1,6 +1,9 @@
 const NUMBER_OF_ROUNDS = 5;
 const display = document.querySelector('.disp_results');
-let roundOut;
+let playerScore = 0;
+let output = "";
+let computerScore = 0;
+
 
 function getRandomInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
@@ -23,13 +26,33 @@ function computerPlay(){
     return move;
 }
 
-function displayResults(string){
-    display.textContent = string;
+function displayResults(string, won){
+    display.textContent = string + " " + playerScore + "-" + computerScore;
 
 }
+function updateScore(won){
+    
+    if(playerScore >= 5 || computerScore >= 5){
+        if(playerScore === 5) output = "Player Won";
+        else output = "Computer Won";
+    }
+    else{
+        switch (won) {
+        case -1:
+            playerScore += 1;
+            if(playerScore === 5) output = "Player Won";
+            break;
+        case 1:
+            computerScore += 1;
+            if(computerScore === 5) output = "Computer Won";        
+            break;
+            
+        }
+    }
+}
+    
 function playRound(playerSelection, computerSelection = computerPlay()){
     playerSelection = playerSelection.toLowerCase(); // insensitive input
-    let output = "";
     let won = false;
     switch (playerSelection) {
         case 'rock':
@@ -82,13 +105,12 @@ function playRound(playerSelection, computerSelection = computerPlay()){
             }
             break;
     }
-    displayResults(output);
+    updateScore(won, output);
+    displayResults(output, won);
     return [output, won];
 }
  
 /*function game(){
-    let playerScore = 0;
-    let computerScore = 0;
 
     for(let i = 0; i < NUMBER_OF_ROUNDS; i++){
         let choice = String(prompt("Choose your weapon"));
